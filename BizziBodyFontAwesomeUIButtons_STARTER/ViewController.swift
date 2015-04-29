b8ef9f1cff8396e14fbd0e106ff6238f3ab1d2bc
bizzi-body <ianbradbury@gmail.com>
2015-04-29 22:01:18 +0100

    Initial commit of tutorial starter solution

---
+++ BizziBodyFontAwesomeUIButtons_STARTER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BizziBodyFontAwesomeUIButtons_STARTER
+//
+//  Created by Ian Bradbury on 29/04/2015.
+//  Copyright (c) 2015 bizzi-body. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
